99242edb66c4c419bb154c710da765d312df0e54
“Kathakoli <kathakolic@gmail.com>
2026-01-12 19:59:52 +0530

    Sum of first n numbers using functional recursion

---
+++ sumfunctional.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int f(int n){
+	if(n==0){
+		return 0;
+	}
+	else{
+		return n+f(n-1);
+	}
+}
+
+int main(){
+	int n;
+	cout << "How many numbers?";
+	cin >> n;
+	cout << "The sum:" << f(n);
+	return 0;
+}
