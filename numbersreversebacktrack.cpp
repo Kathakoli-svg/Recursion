6d6258821c4c141727e9437cf8b6a90d4aaeacf5
“Kathakoli <kathakolic@gmail.com>
2026-01-12 17:16:26 +0530

    print linearly from n to 1 by backtrack

---
+++ numbersreversebacktrack.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void f(int i, int n){
+	if (i>n){
+		return;
+	}
+	f(i+1,n);
+	cout << i << endl;
+}
+
+int main(){
+	int n;
+	cout << "how many numbers:";
+	cin >> n;
+	f(1,n);
+	return 0;
+	
+}
